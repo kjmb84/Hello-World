321670f991995e2a1b23d953b8d65163d3454ca1
Kean <kjmb84@mail.missouri.edu>
2017-09-30 11:15:53 -0500

    whoops

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var helloButton: UIButton!
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func clearLabel(_ sender: UIButton) {
+        helloLabel.text = ""
+    }
+    
+    
+    @IBAction func helloWorldButton(_ sender: UIButton) {
+        helloLabel.text = "Hello World!"
+         
+    }
 
 }
 
